70b84ffc48400718eb86b3f8b0a01b46ffc32dc5
floredo <rbankafe@gmail.com>
2022-01-21 21:37:38 +0000

    fin de creation classe article

--- src/com/projet/poo/models/Article.java
+++ src/com/projet/poo/models/Article.java
@@ -5,20 +5,57 @@
  */
 package com.projet.poo.models;
 
+import java.util.Date;
+
 /**
  *
  * @author Toussain bankafe
  */
 public class Article {
     private String code;
+    private String libelle;
+    private double prix;
+    private int qte;
+    private Date date_creation;
 
     public String getCode() {
         return code;
+    }
+    public String getLibelle(){
+        return libelle;
+    }
+    public double getPrix(){
+        return prix;
+    }
+    public int getQte(){
+        return qte;
+    }
+    public Date getDate_creation(){
+        return date_creation;
     }
 
     public void setCode(String code) {
         this.code = code;
     }
-    
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+    public void setQte(int qte) {
+        this.qte = qte;
+    }
+    public void setDate_creation(Date Date_creation) {
+        this.date_creation = date_creation;
+    }
+
+    public Article(String code, String libelle, double prix, int qte, Date date_creation) {
+        this.code = code;
+        this.libelle = libelle;
+        this.prix = prix;
+        this.qte = qte;
+        this.date_creation = date_creation;
+    }
     
 }
